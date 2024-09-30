cef3b3b54be7ffbb51660552151c1ebb53e08128
Future Computer Training Institute <107338637+fctiadmin@users.noreply.github.com>
2024-09-30 23:30:23 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,58 @@
+// var result=0;
+// for(var i=1;i<=10;i++){
+//     // result+=i;
+//     result=result+i;
+
+// }
+// console.log(result);
+
+
+
+
+// var i=1;
+// while(i<100){
+//     console.log(i);
+//     i++;
+// }
+// var i=1;
+// do{
+// console.log(i);
+// i++;
+// }while(i<100)
+
+// var alphabet=prompt("Enter Your Letter:");
+// switch(alphabet){
+//     case 'A' :
+//     case 'a' :
+//     case 'E' :
+//     case 'e' :
+//     case 'I' :
+//     case 'i' :
+//     case 'O' :
+//     case 'o' :
+//     case 'U' :
+//     case 'u' :
+//     console.log(alphabet+' is Vowel');
+
+//     break;
+//    case 'B':
+//    case 'b':
+//    case 'C':
+//    case 'c':
+//    case 'D':
+//    case 'd':
+//    case 'F':
+//    case 'f':
+//    case 'G':
+//    case 'g':
+//     console.log(alphabet+' is Consonunt');
+// break;
+// default:
+//     console.log(alphabet+' is not Letter');
+// }
+// celcius to fahrenheit program with js
+
+
+// var celcius=prompt("Celcius Value:");
+// var fahrenheit=(celcius * 9/5) + 32;
+// console.log(fahrenheit+' Deg');
